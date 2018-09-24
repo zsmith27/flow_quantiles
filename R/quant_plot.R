821d14462cc76d271212539c3a4a0625f9b311bf
Zachary Smith <zachary.smith1@dec.ny.gov>
2018-09-24 15:40:38 -0400

    Create a function to create a historical flow quantile plot.

--- R/quant_plot.R
+++ R/quant_plot.R
@@ -1,36 +1,86 @@
-library(tidyverse)
-df <- test %>% 
-  filter(datetime < as.Date("2018-01-01")) %>% 
-  quant_summary(flow, day) %>% 
-  gather(quant, flow, quant_00:quant_100) %>% 
-  group_by(quant) %>% 
-  mutate(flow = predict(loess(flow ~ day, span = 0.3))) %>% 
-           ungroup() %>% 
-  spread(quant, flow)
-current <- test %>% 
-  filter(datetime >= as.Date("2018-01-01")) %>% 
-  group_by(day) %>% 
-  summarize(flow = median(flow, na.rm = TRUE)) %>% 
-  ungroup()
+#' @title Historical Flow Quanitle Plot
+#' @description FUNCTION_DESCRIPTION
+#' @param quant.df PARAM_DESCRIPTION
+#' @param x.col PARAM_DESCRIPTION
+#' @param y.col PARAM_DESCRIPTION
+#' @param y.log10 PARAM_DESCRIPTION, Default: TRUE
+#' @param col.pal PARAM_DESCRIPTION, Default: 'rdbu'
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso 
+#'  \code{\link[rlang]{quotation}}
+#'  \code{\link[stringr]{case}}
+#'  \code{\link[dplyr]{tidyeval}}
+#' @rdname gg_quant
+#' @export 
+#' @importFrom rlang enquo
+#' @importFrom stringr str_to_title
+#' @importFrom dplyr quo_name %>% 
 
-x.col <- rlang::quo(day)
-y.col <- rlang::quo(X_00060_00000)
-library(ggplot2)
- my.plot <- 
-  ggplot2::ggplot(df, aes(!!x.col)) +
-  geom_ribbon(aes(ymin = quant_00, ymax = quant_05), fill = "#B2182B") +
-  geom_ribbon(aes(ymin = quant_05, ymax = quant_10), fill = "#EF8A62") +
-  geom_ribbon(aes(ymin = quant_10, ymax = quant_25), fill = "#FDDBC7") +
-  geom_ribbon(aes(ymin = quant_25, ymax = quant_75), fill = "#F7F7F7") +
-  geom_ribbon(aes(ymin = quant_75, ymax = quant_90), fill = "#D1E5F0") +
-  geom_ribbon(aes(ymin = quant_90, ymax = quant_95), fill = "#67A9CF") +
-  geom_ribbon(aes(ymin = quant_95, ymax = quant_100), fill = "#2166AC") +
-  scale_y_log10() +
-  scale_x_continuous(expand = c(0, 0)) +
-  geom_line(data = current, aes(!!x.col, flow))
+gg_quant <- function(quant.df, x.col, y.col, y.log10 = TRUE, col.pal = "rdbu") {
+  # RColorBrewer::brewer.pal(7, "RdBu")
+  # viridis::viridis(7)
+  name.vec <- c(
+    "00-05",
+    "05-10",
+    "10-25",
+    "25-75",
+    "75-90",
+    "90-95",
+    "90-100"
+  )
+  ord.df <- data.frame(legend = name.vec)
+  
+  x.col <- rlang::enquo(x.col)
+  y.col <- rlang::enquo(y.col)
+  final.plot <- quant.df %>% 
+    # mutate(legend = factor(name.vec, level = name.vec)) %>% 
+    ggplot(aes(!!x.col)) +
+    geom_ribbon(aes(ymin = quant_00, ymax = quant_05, fill = "1")) +
+    geom_ribbon(aes(ymin = quant_05, ymax = quant_10, fill = "2")) +
+    geom_ribbon(aes(ymin = quant_10, ymax = quant_25, fill = "3")) +
+    geom_ribbon(aes(ymin = quant_25, ymax = quant_75, fill = "4")) +
+    geom_ribbon(aes(ymin = quant_75, ymax = quant_90, fill = "5")) +
+    geom_ribbon(aes(ymin = quant_90, ymax = quant_95, fill = "6")) +
+    geom_ribbon(aes(ymin = quant_95, ymax = quant_100, fill = "7")) +
+    scale_x_continuous(expand = c(0, 0)) +
+    xlab(stringr::str_to_title(dplyr::quo_name(x.col))) +
+    ylab(stringr::str_to_title(dplyr::quo_name(y.col))) +
+    labs(fill = "Quantile Ranges")
+  
+  if (col.pal == "viridis") {
+    final.plot <- final.plot +
+      scale_fill_manual(labels = rev(name.vec),
+                        breaks = as.character(7:1),
+                        values = c(
+                          "1" = "#440154FF",
+                          "2" = "#443A83FF",
+                          "3" = "#31688EFF",
+                          "4" = "#21908CFF",
+                          "5" = "#35B779FF",
+                          "6" = "#8FD744FF",
+                          "7" = "#FDE725FF"
+                        ))
+  } else {
+    final.plot <- final.plot +
+      scale_fill_brewer(palette = col.pal,
+                        labels = rev(name.vec),
+                        breaks = as.character(7:1))
+  }
 
-plotly::ggplotly(my.plot)
   
+  if (y.log10 == TRUE) {
+    suppressMessages(
+      final.plot <- final.plot + scale_y_log10()
+    )
+  }
   
-  
-RColorBrewer::brewer.pal(7, "RdBu")
+  return(final.plot)
+}
+
